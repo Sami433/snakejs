6e489dc079dd777d94c8de60f3588cf48b6a558e
Sami <saminait@laposte.net>
2023-01-10 23:46:52 +0100

    change

--- snake.js
+++ snake.js
@@ -1,12 +1,10 @@
-// Initialise the canvas and context
 const canvas = document.getElementById("snake-game");
 const ctx = canvas.getContext("2d");
 
-// Set the size of the canvas
 canvas.width = 500;
 canvas.height = 500;
 
-// Set the initial position and size of the snake
+
 let snake = {
   x: canvas.width / 2,
   y: canvas.height / 2,
@@ -16,16 +14,14 @@ let snake = {
   pieces: []
 };
 
-// Create the initial "food" piece
+
 let food = {
   x: 10,
   y: 10
 };
 
-// Set an interval to draw the snake every 100ms
 let gameInterval = setInterval(drawSnake, 100);
 
-// Add event listener to handle snake direction
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight" && snake.direction !== "left") {
     snake.direction = "right";
@@ -39,18 +35,15 @@ document.addEventListener("keydown", (event) => {
 });
 
 function drawSnake() {
-  // Clear the canvas before redrawing the snake
+  
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Go through each piece of
   snake.pieces.forEach((piece) => {
     ctx.fillRect(piece.x, piece.y, snake.size, snake.size);
   });
 
-  // Draw the food on the canvas
   ctx.fillRect(food.x, food.y, snake.size, snake.size);
 
-  // Move the snake based on its current direction
   if (snake.direction === "right") {
     snake.x += snake.speed;
   } else if (snake.direction === "left") {
@@ -61,15 +54,13 @@ function drawSnake() {
     snake.y += snake.speed;
   }
 
-  // Add the current position to the beginning of the snake's pieces array
+ 
   snake.pieces.unshift({ x: snake.x, y: snake.y });
 
-  // Remove the last piece of the snake if it's longer than 1
   if (snake.pieces.length > 1) {
     snake.pieces.pop();
   }
 
-  // Check if the snake is out of bounds
   if (
     snake.x < 0 ||
     snake.x > canvas.width ||
@@ -94,12 +85,10 @@ function drawSnake() {
     }
   }
 
-  // Check if the snake is touching the food
   if (snake.x === food.x && snake.y === food.y) {
-    // Add a new piece to the snake
+
     snake.pieces.push({ x: snake.x, y: snake.y });
 
-    // Move the food to a new random location
     food.x = Math.floor(Math.random() * (canvas.width / snake.size)) * snake.size;
     food.y = Math.floor(Math.random() * (canvas.height / snake.size)) * snake.size;
   }
